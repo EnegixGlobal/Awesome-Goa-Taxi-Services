2276e73fe8f0f58c46260845e18ac81541f7ac3a
kunaldevelopers <kunalking01grd@gmail.com>
2025-08-16 22:43:36 +0530

    added new section 6

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import AboutHero from "../sections/AboutHero.jsx";
 import CompanyIntro from "../sections/CompanyIntro.jsx";
+import WhyChooseUs from "../components/WhyChooseUs.jsx";
 
 export default function About() {
   return (
     <main className="bg-white text-black">
       <AboutHero />
       <CompanyIntro />
+      <div className="bg-brandBlack text-white">
+        <WhyChooseUs />
+      </div>
     </main>
   );
 }
